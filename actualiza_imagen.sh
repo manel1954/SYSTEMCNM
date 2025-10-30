4a43062aa30d1e14228825052c783ef3dc729fc2
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-10-30 23:23:11 +0100

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -4,7 +4,7 @@
                         cd /home/pi/SYSTEM/qt/
                         ./qt_actualizando_imagen &
 
-                        cd /home/pi/SYSTEM3
+                        cd /home/pi/SYSTEMCNM
                         
                         git pull --force                      
 
@@ -15,12 +15,12 @@
 
                         mkdir /home/pi/SYSTEM
                                                 
-                        cp -R /home/pi/SYSTEM3/* /home/pi/SYSTEM                     
+                        cp -R /home/pi/SYSTEMCNM/* /home/pi/SYSTEM                     
                         
                         sudo rm -R /home/pi/SYSTEM/qt
 
                                       
-                        cp -R /home/pi/SYSTEM3/qt/ /home/pi/SYSTEM/
+                        cp -R /home/pi/SYSTEMCNM/qt/ /home/pi/SYSTEM/
 
                         sudo chmod 777 -R /home/pi/SYSTEM
                         
