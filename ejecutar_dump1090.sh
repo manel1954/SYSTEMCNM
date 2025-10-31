61efcfbe6c281a383523eee4aa553abad5f1ca00
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-10-31 23:25:15 +0100

    Update ejecutar_dump1090.sh

--- ejecutar_dump1090.sh
+++ ejecutar_dump1090.sh
@@ -23,3 +23,4 @@ fi
 sed -i "6c Exec=sh ejecutar_dump1090.sh" /home/pi/Desktop/dump1090.desktop
 sed -i "7c Icon=/home/pi/SYSTEM/ICONO_AVION_OFF.png" /home/pi/Desktop/dump1090.desktop
 sed -i "10c Name[es_ES]=Abrir Dump1090" /home/pi/Desktop/dump1090.desktop
+
