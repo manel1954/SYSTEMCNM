937d25feaa7598f67a948a075689daa5c97def1e
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-11-02 10:03:36 +0100

    Update colocar_iconos.sh

--- colocar_iconos.sh
+++ colocar_iconos.sh
@@ -6,10 +6,9 @@ sudo rm *.desktop
 cp /home/pi/SYSTEM/Desktop/*.desktop /home/pi/Desktop
 sudo chmod 777 -R /home/pi/Desktop
 
-# cp /home/pi/icons.screen0-1904x1017.rc /home/pi/.config/xfce4/desktop
-# sudo chmod 777 -R /home/pi/.config/xfce4/desktop
+cp /home/pi/desktop-items-0.conf /home/pi/pcmanfm/LXDE/
 
-cinnamon --replace &
+sudo chmod 777 /home/pi/pcmanfm/LXDE/desktop-items-0.conf 
 
 cd /home/pi/SYSTEM/qt/
 ./qt_colocar_iconos                         
