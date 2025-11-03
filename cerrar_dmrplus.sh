9a8b6cb604b6ddad19733bfe3c12415a91800fa7
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-11-03 18:26:20 +0100

    1

--- cerrar_dmrplus.sh
+++ cerrar_dmrplus.sh
@@ -6,7 +6,7 @@ sudo cp Abrir_MMDVMPLUS.desktop /home/pi
 SCRIPTS_version=$(awk "NR==3" /home/pi/version-fecha-actualizacion)
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sh ejecutar_dmrplus.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_DMRPLUS_OFF.png" /home/pi/Abrir_MMDVMPLUS.desktop
-sed -i "10c Name[es_ES]=Abrir DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "10c Name[es_ES]=DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
 sed -i "6c MMDVMPLUS=OFF" /home/pi/status.ini
 
 cd /home/pi
