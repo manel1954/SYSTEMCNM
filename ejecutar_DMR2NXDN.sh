d5b7da7ea3c24e15753e2556f01c87ca4c2a6a1d
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-11-03 17:56:29 +0100

    Update ejecutar_DMR2NXDN.sh

--- ejecutar_DMR2NXDN.sh
+++ ejecutar_DMR2NXDN.sh
@@ -27,7 +27,7 @@ cd /home/pi/Desktop
 sudo cp Abrir_DMR2NXDN.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh cerrar_DMR2NXDN.sh'" /home/pi/Abrir_DMR2NXDN.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_DMR2NXDN_ON.png" /home/pi/Abrir_DMR2NXDN.desktop
-sed -i "10c Name[es_ES]=Cerrar DMR2NXDN" /home/pi/Abrir_DMR2NXDN.desktop
+sed -i "10c Name[es_ES]=DMR2NXDN" /home/pi/Abrir_DMR2NXDN.desktop
 sed -i "16c DMR2NXDN=ON" /home/pi/status.ini
 cd /home/pi
 sudo cp Abrir_DMR2NXDN.desktop /home/pi/Desktop
@@ -46,7 +46,7 @@ cd /home/pi/Desktop
 sudo cp Abrir_DMR2NXDN.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh ejecutar_DMR2NXDN.sh'" /home/pi/Abrir_DMR2NXDN.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_DMR2NXDN_OFF.png" /home/pi/Abrir_DMR2NXDN.desktop
-sed -i "10c Name[es_ES]=Abrir DMR2NXDN" /home/pi/Abrir_DMR2NXDN.desktop
+sed -i "10c Name[es_ES]=DMR2NXDN" /home/pi/Abrir_DMR2NXDN.desktop
 sed -i "16c DMR2NXDN=OFF" /home/pi/status.ini
 cd /home/pi
 sudo cp Abrir_DMR2NXDN.desktop /home/pi/Desktop
