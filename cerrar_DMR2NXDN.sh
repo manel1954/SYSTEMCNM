0818b8cf6086f8ffdda525fe50b0320f5fdadff8
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-11-03 18:00:46 +0100

    Update cerrar_DMR2NXDN.sh

--- cerrar_DMR2NXDN.sh
+++ cerrar_DMR2NXDN.sh
@@ -8,7 +8,7 @@ sudo cp Abrir_DMR2NXDN.desktop /home/pi
 
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh ejecutar_DMR2NXDN.sh'" /home/pi/Abrir_DMR2NXDN.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICO_DMR2NXDN_OFF.png" /home/pi/Abrir_DMR2NXDN.desktop
-sed -i "10c Name[es_ES]=Abrir DMR2NXDN" /home/pi/Abrir_DMR2NXDN.desktop
+sed -i "10c Name[es_ES]=DMR2NXDN" /home/pi/Abrir_DMR2NXDN.desktop
 sed -i "16c DMR2NXDN=OFF" /home/pi/status.ini
 
 cd /home/pi
